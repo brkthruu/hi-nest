a4ecd97010739b71b505ba4d2e445267d2c64e5d
hysimok <jeonghy95@gmail.com>
2021-08-02 04:40:01 +0900

    add AuthGuard
    Controller에서 login 할 수 있는 경로를 생성한 후
    AuthGuard 이용해 passport-local strategy 와 연결

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller('')
 export class AppController {
   @Get()
   home() {
     return 'Welcome to my Movie API';
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
