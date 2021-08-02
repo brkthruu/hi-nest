8c2821b531b05c7484896032321c9ab1600c8550
hysimok <jeonghy95@gmail.com>
2021-08-02 19:22:11 +0900

    JWT Authentication
    passport-jwt strategy를 활용해 Access token을 발급받고 활용해보기
    발급받은 토큰을 이용해 profile 정보를 불러온다.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller('')
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @Get()
   home() {
     return 'Welcome to my Movie API';
@@ -11,6 +15,12 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
