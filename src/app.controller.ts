9d26083cddf55a68abef7dc17b11c1cdd192396c
hysimok <jeonghy95@gmail.com>
2021-08-06 17:11:07 +0900

    rearrange endpoint path
    app.controller에서 관리하던 경로들을 각 모듈에 맞게 재배치

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,9 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('')
 export class AppController {
-  constructor(private authService: AuthService) {}
-
   @Get()
   home() {
     return 'Welcome to my Movie API';
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
   }
 }
